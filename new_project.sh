973c658a8d1eaeaea505c9f103d9ac120f3d6434
kesh25 <kinyua.dn@outlook.com>
2024-07-10 14:30:33 +0300

    test

---
+++ new_project.sh
@@ -0,0 +1,23 @@
+
+echo "<< Creating a new git project >> \n"
+
+git init 
+
+echo "Type a descriptive message about the commit: \n"
+read commit_message
+
+git add *
+git commit -m $commit_message
+
+
+echo "Enter remote GIT url": 
+
+read remote_url
+
+git remote add origin $remote_url
+git branch -M main
+git push -u origin main 
+
+
+
+echo "Github repo created!"
