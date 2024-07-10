99627bece3a062dd3dd0d91c5aac8a1e61e59adf
kesh25 <kinyua.dn@outlook.com>
2024-07-10 14:34:03 +0300

    Initial commit

--- new_project.sh
+++ new_project.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 echo "<< Creating a new git project >> \n"
 
@@ -6,15 +7,16 @@ git init
 echo "Type a descriptive message about the commit: \n"
 read commit_message
 
+echo "$commit_message"
 git add *
-git commit -m $commit_message
+git commit -m "$commit_message"
 
 
 echo "Enter remote GIT url": 
 
 read remote_url
 
-git remote add origin $remote_url
+git remote add origin "$remote_url"
 git branch -M main
 git push -u origin main 
 
